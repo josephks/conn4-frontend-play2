26f16a98ca45757ea57076690253c254b2a20ce0
Joseph Shraibman <jks@iname.com>
2014-05-16 15:41:27 -0400

    Infrastructure for prerendering

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,10 +5,16 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action { request =>
+  def withDims(cols: Int, rows: Int) = {
+     index(Some(cols, rows))
+  }
+
+  def default() = { index(None) }
+
+  def index(dims: Option[(Int, Int)]) = Action { request =>
     implicit val req = request
     System.err.println("body class is: "+req.body.getClass())
-    val inner = views.html.index()
+    val inner = views.html.index(dims)
     Ok(views.html.main("title", List("index.css"))(inner))
   }
 
